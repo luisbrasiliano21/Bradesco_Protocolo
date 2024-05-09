6379e89d43c9c680db6c275dae584e224894c0fb
Raniel Luna <raniielluna@gmail.com>
2024-05-08 23:43:53 -0300

    Modificar busca

--- src/main/java/com/Bradesco/demo/Controllers/protocolo_Controll.java
+++ src/main/java/com/Bradesco/demo/Controllers/protocolo_Controll.java
@@ -1,4 +1,5 @@
 package com.Bradesco.demo.Controllers;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Bradesco.demo.entity.Consulta_Protocolo;
-
 import com.Bradesco.demo.Repository.Protocolo_repository;
-
 
 @RestController
 @RequestMapping(value="/Protocolos")
 public class protocolo_Controll {
-	
+
 	@Autowired
 	private Protocolo_repository repository;
 
@@ -24,10 +23,10 @@ public class protocolo_Controll {
 		List<Consulta_Protocolo> Protocolos = repository.findAll();
 		return Protocolos;
 	}
-	
- 	@GetMapping(value = "/{id}")
-	public Consulta_Protocolo findByID(@PathVariable Long id) {
-		Consulta_Protocolo result = repository.findById(id).get();
+
+	@GetMapping(value = "/numero/{numeroProtocolo}")
+	public Consulta_Protocolo findByNumeroProtocolo(@PathVariable Long numeroProtocolo) {
+		Consulta_Protocolo result = repository.findByNumeroProtocolo(numeroProtocolo);
 		return result;
- 	}
+	}
 }
