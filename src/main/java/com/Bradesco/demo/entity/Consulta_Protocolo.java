99480b9feb0402d46cde3106ff5d47687de21440
Raniel Luna <raniielluna@gmail.com>
2024-05-11 13:20:31 -0300

    Adicionando Import Lombok

--- src/main/java/com/Bradesco/demo/entity/Consulta_Protocolo.java
+++ src/main/java/com/Bradesco/demo/entity/Consulta_Protocolo.java
@@ -7,7 +7,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_Protocolo")
